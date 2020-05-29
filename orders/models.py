55b543ffd62a856164a4440ec7ce5cb71cb73e9d
CKOT666 <nikolaybelikov23@gmail.com>
2020-05-29 12:41:23 +0200

    Идём дальше

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import ForeignKey
 from products.models import Product
+from django.db.models.signals import post_save
 
 
 class Status(models.Model):
@@ -10,7 +11,7 @@ class Status(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "Статус {1}".format(self.name)
+        return "Статус {0}".format(self.name)
 
     class Meta:
         verbose_name = 'Статус'
@@ -29,11 +30,15 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "Заказ {1} {2}".format(self.status, self.id)
+        return str("Заказ {0} {1}".format(self.status.name, self.id))
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
+
+    def save(self, *args, **kwargs):
+
+        super(Order, self).save(*args, **kwargs)
 
 
 class ProductInOrder(models.Model):
@@ -47,8 +52,41 @@ class ProductInOrder(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def save(self, *args, **kwargs):
+        price_per_item = self.product.price
+        self.price_per_item = price_per_item
+
+        self.total_price = self.nmb * price_per_item
+
+        order = self.order
+        all_product_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+        order_total_price = 0
+        for item in all_product_in_order:
+            order_total_price += item.total_price
+
+        self.order.total_price = order_total_price
+        self.order.save(force_update=True)
+
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+        order = instance.order
+        all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+        order_total_price = 0
+        for item in all_products_in_order:
+            order_total_price += item.total_price
+
+        instance.order.total_price = order_total_price
+        instance.order.save(force_update=True)
+
+
+post_save.connect(product_in_order_post_save, sender=ProductInOrder)
